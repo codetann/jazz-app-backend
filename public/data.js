4fe6a213776684f7cc090a058b66cb1e9d640af0
codetann <thetannerthomas@gmail.com>
2021-01-08 14:56:09 -0700

    temp data file for photos

---
+++ public/data.js
@@ -0,0 +1,20 @@
+export const colors = [
+  "#FFB941",
+  "#F07433",
+  "#D5223E",
+  "#BB4048",
+  "#B14D4D",
+  "#5C2C2D",
+  "#0A0C0D",
+];
+
+export const imgURL = {
+  donovan:
+    "https://ak-static.cms.nba.com/wp-content/uploads/silos/nba/latest/440x700/1628378.png",
+  rudy:
+    "https://ak-static.cms.nba.com/wp-content/uploads/silos/nba/latest/440x700/203497.png",
+  mike:
+    "https://ak-static.cms.nba.com/wp-content/uploads/silos/nba/latest/440x700/201144.png",
+  royce:
+    "https://ak-static.cms.nba.com/wp-content/uploads/silos/nba/latest/440x700/1626220.png",
+};
