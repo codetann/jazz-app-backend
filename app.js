c4db598726c440db8b0e6f22ff724f3d4601831a
codetann <thetannerthomas@gmail.com>
2021-01-09 12:03:01 -0700

    add stats route

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-const { json } = require("express");
 const express = require("express");
+const path = require("path");
 const app = express();
 
+const homeRoute = require("./routes/homeRoute");
 const streamRoute = require("./routes/streamRoute");
 const scheduleRoute = require("./routes/scheduleRoute");
+const statsRoute = require("./routes/statsRoute");
 
 // middleware
-app.use(json());
+app.use(express.json());
+app.use(express.static("public"));
 
 // routes
+app.use("/api", homeRoute);
 app.use("/api", streamRoute);
 app.use("/api", scheduleRoute);
+app.use("/api", statsRoute);
 
 // server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`[server] running on port ${PORT}`));
+
+/*
+    TODO: add a stats route per player
+    TODO: add any time functionality route
+    TODO: add readme file
+*/
