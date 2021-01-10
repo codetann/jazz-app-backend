07c84f5288d9ba21b40a7fa4d2fcb87f0bcb663a
codetann <thetannerthomas@gmail.com>
2021-01-10 14:16:20 -0700

    removed https link

--- routes/streamRoute.js
+++ routes/streamRoute.js
@@ -4,7 +4,7 @@ const axios = require("axios").default;
 const cheerio = require("cheerio");
 
 router.get("/stream", (req, res) => {
-  const url = "https://nbastream.tv/utah-jazz-live-stream/";
+  const url = "http://nbastream.tv/utah-jazz-live-stream/";
 
   // axios fetchting url
   axios
@@ -12,8 +12,7 @@ router.get("/stream", (req, res) => {
     .then((body) => {
       // initiate cheerio to grab link from nbastreams.tv
       const $ = cheerio.load(body.data);
-      const httpLink = $(".videoWrapper iframe").attr("src");
-      const link = httpLink.slice(0, 4) + "s" + httpLink.slice(4);
+      const link = $(".videoWrapper iframe").attr("src");
       res.send({ link: link });
     })
     .catch((error) => console.log(error));
