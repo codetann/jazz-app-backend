9aff54d1f585da3e82c2bdbeae6e43d1b64a4177
codetann <thetannerthomas@gmail.com>
2021-01-10 12:56:56 -0700

    remove cors

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const scheduleRoute = require("./routes/scheduleRoute");
 const statsRoute = require("./routes/statsRoute");
 
 // middleware
-app.use(cors());
+
 app.use(express.json());
 app.use(express.static("public"));
 
